551ad0427654cc7e9fe73a52a78520cebbfd259a
zhaoshiquan <zhaoshiquan@nonobank.com>
2017-12-06 19:46:01 +0800

    实现二叉树兄弟节点获取

--- src/com/qqmaster/com/BrotherToBinaryTree.java
+++ src/com/qqmaster/com/BrotherToBinaryTree.java
@@ -1,7 +1,36 @@
 package com.qqmaster.com;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class BrotherToBinaryTree {
 	
+	
+	
+	public static Node doBrother(Node t){
+		Queue<Node> q = new ArrayDeque<Node>();
+		if(t == null){
+			q.add(t);
+		}else {
+			return t;
+		}
+		
+		Node nt = new Node();
+		while(!q.isEmpty()){
+			Node mid = q.poll();
+			nt.brother = mid;
+			
+			if(mid.left != null){
+				q.add(mid.left);
+			}
+			
+			if(mid.right != null){
+				q.add(mid.right);
+			}
+		}
+		
+		return nt;
+	}
 }
 
 class Node{
