2186bd049e1b37cd9f9c096c6359f40f312816bb
zhaoshiquan <zhaoshiquan@nonobank.com>
2017-12-13 16:05:42 +0800

    添加两个链表的交点

---
+++ src/com/qqmaster/com/ListNode.java
@@ -0,0 +1,13 @@
+package com.qqmaster.com;
+
+public class ListNode {
+	public ListNode next;
+	
+	public String data;
+	
+	public ListNode(){}
+	
+	public ListNode(String data){
+		this.data = new String(data);
+	}
+}
